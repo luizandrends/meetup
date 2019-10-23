2048b8df3260621ddca1c25bbcb65d0b357dbdb1
Luiz Andre Nunes da Silva <luizxd234@gmail.com>
2019-10-23 17:35:30 -0300

    chore: Adding new dependencies

--- meetupmobile/src/App.js
+++ meetupmobile/src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Text } from 'react-native';
 
 // import { Container } from './styles';
 
+import Routes from './routes';
+
 export default function App() {
-  return <Text>Hello Word</Text>;
+  return <Routes />;
 }
